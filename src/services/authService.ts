830c7fb57069313cc81848b12ab7055c8232a91e
Ahmad Ginanjar <ahmad.ginanjar.m@gmail.com>
2024-05-27 13:51:04 +0700

    refactor: Update createUser function to use user model functions

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,21 +1,20 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import { createUser as add, CreateUserInput, findUserByEmail } from '../models/user';
 
-const prisma = new PrismaClient();
 
 export const createUser = async (data: { email: string; password: string }) => {
   const hashedPassword = await bcrypt.hash(data.password, 10);
-  return prisma.user.create({
-    data: {
-      email: data.email,
-      password: hashedPassword,
-    },
-  });
+  const payload: CreateUserInput = {
+    email: data.email,
+    password: hashedPassword,
+  };
+
+  return add(payload);
 };
 
 export const loginUser = async (data: { email: string; password: string }) => {
-  const user = await prisma.user.findUnique({ where: { email: data.email } });
+  const user = await findUserByEmail(data.email);
   if (!user) throw new Error('Invalid email or password');
 
   const isPasswordValid = await bcrypt.compare(data.password, user.password);
