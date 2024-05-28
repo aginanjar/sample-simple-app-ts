8fdb6e3c1aba0d7cac01bd3fde0cb253d1895cbc
Ahmad Ginanjar <ahmad.ginanjar.m@gmail.com>
2024-05-28 22:05:58 +0700

    Wired up user routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
 import profileRoutes from './routes/profileRoutes';
+import userRoutes from './routes/userRoutes';
 import purchaseRoutes from './routes/purchaseRoutes';
 import { errorMiddleware } from './middleware/errorMiddleware';
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/profiles', profileRoutes);
 app.use('/api/purchase', purchaseRoutes);
 
